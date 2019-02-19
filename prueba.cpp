520d3016e9e10e5f4567e35a76e3fe1ee704840b
Angel Barrera <angel.barrera@lakmisystems.com.mx>
2019-02-19 15:42:28 -0600

    Me falto agregar el archivo nuevo jeje
    este es es archivo que reemplaza al archivo pasado

---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	printf("Hola comunidad de GitHub\n\n");
+	system("pause");
+	system("cls");
+	printf("Hasta la próxima amigos\n\n");
+	system("pause");
+}
