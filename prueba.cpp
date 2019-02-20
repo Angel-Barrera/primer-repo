fa81088d325675ea788e40289b68ad2845b7eef3
Angel Barrera <47359209+Angel-Barrera@users.noreply.github.com>
2019-02-20 17:19:08 -0600

    mensaje intermedio
    Se agregó un mensaje intermedio  en el código fuente, para comprobar que los commits se visualizan también en el pull request local

--- prueba.cpp
+++ prueba.cpp
@@ -5,6 +5,9 @@ int main(){
 	printf("Hola comunidad de GitHub\n\n");
 	system("pause");
 	system("cls");
+	printf("Utilizo todas las bondades de GiHub\n\n");
+	system("pause");
+	system("cls");
 	printf("Hasta la proxima amigos\n\n");
 	return(0);
 }
