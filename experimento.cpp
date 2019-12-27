71c56806415b66ce8f30d43b03913951a297fb00
Angel Barrera <47359209+Angel-Barrera@users.noreply.github.com>
2019-12-27 17:25:18 -0600

    archivo creado desde GH

---
+++ experimento.cpp
@@ -0,0 +1,10 @@
+include <stdio.h>
+include <stdlib.h>
+
+int main(){
+	printf("Cambio desde GitHub\n\n");
+	system("pause");
+	system("cls");
+	printf("Hasta la proxima amigos\n\n");
+	return(0);
+}
